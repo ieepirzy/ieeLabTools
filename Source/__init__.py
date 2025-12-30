6f29c6235a1beba1465e75fd7578171b9a59096a
ieepirzy <ilari.e.pirkkalainen@student.jyu.fi>
2025-12-30 14:57:38 +0200

    refactored for clarity

--- Source/__init__.py
+++ Source/__init__.py
@@ -1,3 +1,20 @@
-from .core import Yvel, weightedLinregress, ortDistanceRegress
+"""
+ieeLabTools
+===========
 
-__all__ = ["Yvel", "weightedLinregress","ortDistanceRegress"]
+Tools for symbolic and numeric uncertainty propagation
+and weighted linear regression for laboratory data analysis.
+"""
+
+
+from .core import (
+    Yvel,
+    WeightedLinregress,
+)
+
+__all__ = [
+    "Yvel",
+    "WeightedLinearRegression",
+]
+
+__version__ = "0.1.2"
